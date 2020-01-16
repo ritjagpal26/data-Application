48e54b1a830504e5df61714bc9f534dfc2bc406d
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-01-16 17:10:30 -0500

    Initial Commit

---
+++ data Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  data Application
+//
+//  Created by Mac on 2020-01-16.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
